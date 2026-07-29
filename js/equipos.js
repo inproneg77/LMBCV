719da8d6cb943380a6687d33016ba6911f05546f
inproneg77 <inproneg@gmail.com>
2026-07-28 18:01:09 -0700

    Add files via upload

--- js/equipos.js
+++ js/equipos.js
@@ -123,7 +123,10 @@ function renderDetalleEquipo(equipoId) {
 
   cont.innerHTML = `
     <button class="volver-btn" id="volver-equipos">← Todos los equipos</button>
-    <button class="pdf-btn no-print" id="exportar-pdf">🖨️ Exportar a PDF</button>
+    <button class="pdf-btn no-print" id="exportar-pdf">
+      <svg width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"/><polyline points="14 2 14 8 20 8"/><line x1="12" y1="18" x2="12" y2="12"/><polyline points="9 15 12 18 15 15"/></svg>
+      Exportar a PDF
+    </button>
 
     <div class="equipo-detalle-header">
       <img src="${RUTA_IMG}${equipo.logo}" alt="${equipo.nombre}">
